f4b58a1e17f551b2dbd011011f9abd3dd765cc17
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2020-01-15 12:20:11 +0100

    show custom switch into main activity providing it with attributes

--- app/src/main/java/com/pedroroig/androidcustomswitch/MainActivity.kt
+++ app/src/main/java/com/pedroroig/androidcustomswitch/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.pedroroig.androidcustomswitch
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        custom_switch.setOnSwitchChangeListener {
+            toast(if(it) "Right" else "Left")
+        }
     }
 }
+
+fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, length).show()
+}
+
